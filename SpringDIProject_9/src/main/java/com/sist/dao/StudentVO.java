55d3a9d9180851f4358e3c271b93a5e0fdd45964
chominseok1 <mins2713@naver.com>
2023-07-26 17:10:33 +0900

    07-26

---
+++ SpringDIProject_9/src/main/java/com/sist/dao/StudentVO.java
@@ -0,0 +1,27 @@
+package com.sist.dao;
+
+import lombok.Getter;
+import lombok.Setter;
+/*
+ * CREATE TABLE student(
+	    hakbun NUMBER,
+	    name VARCHAR2(51) CONSTRAINT std_name_nn NOT NULL,
+	    kor NUMBER,
+	    eng NUMBER,
+	    math NUMBER,
+	    CONSTRAINT std_hakbun_pk PRIMARY KEY(hakbun)
+	);
+ */
+/*
+ * HAKBUN NOT NULL NUMBER       
+	NAME   NOT NULL VARCHAR2(51) 
+	KOR             NUMBER       
+	ENG             NUMBER       
+	MATH            NUMBER       
+ */
+@Getter // 읽기
+@Setter // 쓰기 
+public class StudentVO {
+	private int hakbun,kor,eng,math;
+	private String name; 
+}
