80fac366ee1ccf2016a0bd5030a26ec7587fbc97
chominseok1 <mins2713@naver.com>
2023-08-03 17:05:08 +0900

    08-03

---
+++ SpringTilesProject_2/src/main/java/com/sist/commons/CommonException.java
@@ -0,0 +1,40 @@
+package com.sist.commons;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+// Spring : 반복코딩(X)
+// 모든 Controller의 예외처리 => 전역 
+@ControllerAdvice
+public class CommonException {
+	@ExceptionHandler(RuntimeException.class)
+	public void runtimeExecption(RuntimeException ex)
+	{
+		System.out.println("====== RuntimeException =====");
+		ex.printStackTrace();
+		System.out.println("==============================");
+	}
+	@ExceptionHandler(SQLException.class)
+	public void sqlException(SQLException ex)
+	{
+		System.out.println("====== SQLException =====");
+		ex.printStackTrace();
+		System.out.println("==============================");
+	}
+	@ExceptionHandler(IOException.class)
+	public void ioExecption(IOException ex)
+	{
+		System.out.println("====== IOException =====");
+		ex.printStackTrace();
+		System.out.println("==============================");
+	}
+	@ExceptionHandler(Exception.class)
+	public void execption(Exception ex)
+	{
+		System.out.println("====== Exception =====");
+		ex.printStackTrace();
+		System.out.println("==============================");
+	}
+}
