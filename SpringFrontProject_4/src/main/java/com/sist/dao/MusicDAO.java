8701d8462f300222412e6865c52f432c4f3f6f77
chominseok1 <mins2713@naver.com>
2023-08-10 16:33:20 +0900

    08-10

---
+++ SpringFrontProject_4/src/main/java/com/sist/dao/MusicDAO.java
@@ -0,0 +1,24 @@
+package com.sist.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import com.sist.mapper.*;
+import com.sist.vo.*;
+
+@Repository
+public class MusicDAO {
+	@Autowired
+	private MusicMapper mapper;
+	//@Select("SELECT * FROM genie_music WHERE cno=#{cno}")
+	  public List<MusicVO> musicListData(int cno)
+	  {
+		  return mapper.musicListData(cno);
+	  }
+	  public MusicVO musicDetailData(int mno)
+	  {
+		  return mapper.musicDetailData(mno);
+	  }
+}
